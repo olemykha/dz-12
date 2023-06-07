ff867199a7a40274c5a8cf2d7ec088161aa73c6c
olemykha <alex.mkhlk@gmail.com>
2023-06-07 10:36:14 +0000

    add hm12 with solving the problems indicated in the comments

--- src/test/java/person/PersonTests.java
+++ src/test/java/person/PersonTests.java
@@ -9,19 +9,25 @@ import org.testng.asserts.SoftAssert;
 public class PersonTests {
 
     @Test(dataProvider = "personsData", dataProviderClass = TestDataProvider.class)
-    public void testIsRetired(Man man, Woman woman){
-        String maidenName = woman.getLastName();
+    public void testIsRetired(Man man, Woman woman) {
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(man.isRetired(), man.getAge() >= 65, "The status is incorrect");
-        softAssert.assertEquals(woman.isRetired(), woman.getAge() >= 60, "The status is incorrect");
+        softAssert.assertTrue(man.isRetired() == (man.getAge() >= 65), "The man's retirement status is incorrect");
+        softAssert.assertTrue(woman.isRetired() == (woman.getAge() >= 60), "The woman's retirement status is incorrect");
+        softAssert.assertAll();
+    }
+
+    @Test(dataProvider = "personsData", dataProviderClass = TestDataProvider.class)
+    public void testRegisterPartnership(Man man, Woman woman) {
+        String maidenName = woman.getLastName();
         woman.registerPartnership(man);
+        SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(woman.getPartner(), man, "The partner is incorrect");
         softAssert.assertEquals(man.getPartner(), woman, "The partner is incorrect");
         softAssert.assertEquals(woman.getLastName(), man.getLastName(), "The last name is incorrect");
         man.deregisterPartnership();
-        softAssert.assertEquals(woman.getPartner(), null, "The partner exists but should not");
-        softAssert.assertEquals(man.getPartner(), null, "The partner exists but should not");
-        softAssert.assertEquals(woman.getLastName(), maidenName, "The maiden name is incorrect" );
+        softAssert.assertNull(woman.getPartner(), "The partner exists but should not");
+        softAssert.assertNull(man.getPartner(), "The partner exists but should not");
+        softAssert.assertEquals(woman.getLastName(), maidenName, "The maiden name is incorrect");
         softAssert.assertAll();
     }
 }
